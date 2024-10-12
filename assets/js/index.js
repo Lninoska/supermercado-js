74d4c26a21b90ec91f48869521c3b8c8bb6b3e91
Lorena <lorena.n.barrioz@gmail.com>
2024-10-11 22:16:32 -0300

    algunos cambios

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
 class Producto {
-    constructor(nombre, precio) {
+    constructor(nombre, precio, cantidad = 1) {
         this.nombre = nombre;
         this.precio = precio;
+        this.cantidad = cantidad;
     }
 }
 
@@ -13,20 +14,21 @@ class carrito {
         this.almacen.push(producto);
     }
     calculartotal() {
-        return this.carrito.reduce((total, producto) => total + producto.precio, 0)
+        return this.almacen.reduce((total, producto) => total + producto.precio, 0)
     }
     finalizarcompra() {
     }
     detallescompra() {
         console.log("Detalles de compra: ");
         this.almacen.forEach(producto => {
-            console.log(`Producto: ${producto.nombre}, Precio: ${producto.precio}`)
+            console.log(`Producto: ${producto.nombre}, Precio: ${producto.precio}, cantidad: ${producto.cantidad}`)
         });
         console.log(`Total de la compra: ${this.calculartotal()}`)
     }
 }
 
 const tienda = new carrito();
+let totalcompra = 0;
 let productosdisponibles = [
     new Producto("Leche", 1000),
     new Producto("Pan de molde", 2000),
@@ -41,9 +43,32 @@ while (true) {
         productosLista += `${i + 1}. ${producto.nombre} - $${producto.precio}\n`;
     });
 
-    const seleccion = prompt(`Productos disponibles:\n${productosLista}\n o ingrese 'fin' para finalizar la compra` );
+    const seleccion = prompt(`Productos disponibles:\n${productosLista}\n Ingrese el número del producto que deseas agregar al carrito 
+        \n o ingrese 'fin' para finalizar la compra`);
     if (seleccion === "fin") {
         break;
     }
+    const seleccionnum = parseInt(seleccion);
+    if( !isNaN(seleccionnum) && seleccionnum > 0 && seleccionnum <= productosdisponibles.length){
+        const productoseleccionado = productosdisponibles [seleccion - 1];
+        tienda.agregarproducto(productoseleccionado);
+        totalcompra += productoseleccionado.precio;
+        alert(`${productoseleccionado.nombre} ha sido agregado al carrito`);
+
+        const cantidad = parseInt (prompt (`Ingrese la cantidad de ${productoseleccionado.nombre} que desea`))
+        productoseleccionado.cantidad = cantidad;
+        totalcompra += productoseleccionado.precio * cantidad; 
+
+        const continuar = confirm(`Total actual de su compra es: $${totalcompra}.\n ¿Desea agregar otro producto al carrito?`);
+        if(!continuar){
+            break;
+        }
+    } else {
+        alert("Seleción no valida. Por favor, seleccione un producto disponible");
+    }
 
 }
+
+alert(`Total de su compra es ${totalcompra}`)
+tienda.detallescompra();
+tienda.finalizarcompra();
